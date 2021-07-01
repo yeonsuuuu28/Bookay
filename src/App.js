5925a043cbdaaa6c4f32574d50911f88dda9ca88
heesu7212 <yoonghethsiu@gmail.com>
2021-07-01 22:36:47 +0700

    navigation bar started!

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      BOOKAY!!
+      BOOKAY!!!
+      <br />
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Book List</a></li>
+        <li><a href="">Journal</a></li>
+        <li><a href="">Log In</a></li>
+      </ul>
     </div>
   );
 }
